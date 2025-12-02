eebdb1ddf554bed40cf76c7688b3765f202595d6
Ayush Sharma <160806563+ayuxharma@users.noreply.github.com>
2025-12-02 09:02:07 +0000

    First commit of server

---
+++ server.c
@@ -0,0 +1,27 @@
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <regex.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 104857600
+
+const char *get_file_extension(const char*file_name) {
+    const char *dot = strrchr (file_name, '.') ;
+    if (!dot || dot == file_name) {
+        return "" ;
+    }
+    return dot + 1 ;
+}
